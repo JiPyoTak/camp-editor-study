35e69898607475457317f7a788f9573a226a8f94
jintak0401 <jintak0401@naver.com>
2023-01-17 21:38:33 +0900

    first commit

---
+++ js/campEditor/types.ts
@@ -0,0 +1,49 @@
+type ToolbarOption =
+  | 'undo'
+  | 'redo'
+  | 'font'
+  | 'font-size'
+  | 'bold'
+  | 'underline'
+  | 'italic'
+  | 'strike'
+  | 'subscript'
+  | 'superscript'
+  | 'remove-format'
+  | 'heading'
+  | 'font-color'
+  | 'font-background-color'
+  | 'code-block'
+  | 'inline-code-block'
+  | 'indent'
+  | 'outdent'
+  | 'order-list'
+  | 'unorder-list'
+  | 'align'
+  | 'horizontal-line'
+  | 'line-height'
+  | 'table'
+  | 'image'
+  | 'video'
+  | 'preview'
+  | 'show-block'
+  | 'full-screen'
+  | 'help';
+
+type ToolbarButtonInfo = {
+  icon: string;
+  tagName?: keyof HTMLElementTagNameMap;
+};
+
+type Toolbars = {
+  [key in ToolbarOption]?: ToolbarButtonInfo;
+};
+
+interface EditorOption {
+  width?: number | string;
+  height?: number | string;
+  placeholder?: string;
+  toolbarOptions: ToolbarOption[][];
+}
+
+export type { EditorOption, ToolbarOption, ToolbarButtonInfo, Toolbars};
---
+++ js/index.js
@@ -0,0 +1,3 @@
+import { CampEditor } from './campEditor/campEditor.ts';
+new CampEditor('editor', { width: 1000, height: 400, placeholder: "적으시던가", toolbarOptions: [['bold', 'italic', 'underline', 'strike']] });
+//# sourceMappingURL=index.js.map
