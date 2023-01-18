5a7c9f7758a7f50783529cdc68f3f229f7eae8e4
seokho10007 <sukho1007@naver.com>
2023-01-18 14:19:06 +0900

    사용하지 않는 폴더 삭제

--- js/index.js
+++
@@ -1,3 +0,0 @@
-import { CampEditor } from './campEditor/campEditor.ts';
-new CampEditor('editor', { width: 1000, height: 400, placeholder: "적으시던가", toolbarOptions: [['bold', 'italic', 'underline', 'strike']] });
-//# sourceMappingURL=index.js.map
